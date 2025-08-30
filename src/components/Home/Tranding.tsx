cd903c994ebc05383ba61bf4d44fa03a494d64a0
Jigar Prajapati <jigarprajapati6457@gmail.com>
2025-08-30 22:07:56 +0530

    final update

--- src/components/Home/Tranding.tsx
+++ src/components/Home/Tranding.tsx
@@ -39,7 +39,7 @@ function Tranding() {
       </div>
 
       {/* Product Grid */}
-      <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-items-center">
         {products.map((product, index) => (
           <ProductCard
             key={index}
